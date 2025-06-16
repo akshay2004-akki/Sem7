9c0c096f4304d15252a425fdfe7df66b42231b75
Akshay anand <akshay1622004@gmail.com>
2025-06-17 01:16:28 +0530

    feat: add link field to notification schema and create initial files for user controller, auth middleware, multer middleware, user routes, and cloudinary utility

--- server/src/models/notifiation.model.js
+++ server/src/models/notifiation.model.js
@@ -10,6 +10,10 @@ const notificationSchema = new Schema({
         type: String,
         required: true
     },
+    link : {
+        type: String,
+        required: true
+    },
     seen: {
         type: Boolean,
         default: false
