7b10dbee7b88db786079f0ca2e0eac56d3bbe0b2
Akshay anand <akshay1622004@gmail.com>
2025-10-05 12:30:10 +0530

    feat: Remove unused activity and study hours models

--- server/src/models/activity.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const activitySchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  message: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-export const Activity = mongoose.model("Activity", activitySchema);
--- server/src/models/studyhr.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const studyHoursSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  day: { type: String, required: true }, // Mon, Tue, etc.
-  hours: { type: Number, default: 0 },
-});
-
-export const StudyHours =  mongoose.model("StudyHours", studyHoursSchema);
